a4a280fce57b2156e4f967820e45f37b4b6b6168
Erez Mizrahi <erezmizra@gmail.com>
2023-04-02 01:04:59 +0300

    Fixed metallb (#2)
    * Fixed metallb
    
    * fix

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -5,3 +5,32 @@ resource "helm_release" "argocd" {
   cleanup_on_fail  = true
   create_namespace = true
 }
+
+resource "kubectl_manifest" "metallb" {
+  yaml_body = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: metallb
+  namespace: argocd
+spec:
+  project: default
+  source:
+    repoURL: https://github.com/Looty/homelab.git
+    targetRevision: HEAD
+    path: manifests/charts/metallb
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: metallb-system
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - CreateNamespace=true
+YAML
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
